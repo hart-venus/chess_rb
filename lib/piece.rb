43ffe80dba40d4f81d1e1e11702a4c24ded90dbd
Ariel Leyva <aleyva1509@gmail.com>
2022-03-20 09:10:48 -0600

    base piece class

--- lib/piece.rb
+++ lib/piece.rb
@@ -1,3 +1,15 @@
-class Piece
+# frozen_string_literal: true
 
+# base piece class for all other pieces to inherit from.
+class Piece
+  attr_accessor :x_coord, :y_coord, :id, :is_white
+
+  def initialize(id, x_coord = 0, y_coord = 0, is_white: true)
+    @x_coord = x_coord
+    @y_coord = y_coord
+    @id = id
+    @is_white = is_white
+  end
 end
+
+# pawn piece
